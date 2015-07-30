7f3e8110e8c0cbf69ce43613576da2bf4a684470
Terry S <itsterry@gmail.com>
2015-07-31 00:43:11 +0100

    Moved required database fields to class level

--- lib/morse_fields_validator/fields_validator.rb
+++ lib/morse_fields_validator/fields_validator.rb
@@ -10,23 +10,20 @@ module FieldsValidator
     def validate_required_attributes
       required_attributes.each do |a|
         validates a.to_sym, presence: true 
-      end
-    end
-    def load_required_attributes(*_attrs)
-      @required_attributes=_attrs.any? ? _attrs : []
-      @required_attributes.uniq!
+      end if required_attributes.any?
     end
 
     def required_attributes
-      @required_attributes
+      defined?(super) ? super : []
     end
+
+    def required_database_fields
+      defined?(super) ? super : []
+    end
+
   end
 
   private
-
-  def required_database_fields
-    defined?(super) ? super : []
-  end
 
   def validate_column_names
     required_database_fields.each { |rdf| errors_add(rdf, "does not have a column in the database.")  unless self.class.column_names.include?(rdf)} if required_database_fields.any?
