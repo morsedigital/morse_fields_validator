89f7a04983a5302865ad91815cef8a4dce8db92d
Terry S <itsterry@gmail.com>
2015-07-30 01:27:47 +0100

    Made validator fire on model validation, not instantiation

--- lib/morse_fields_validator/fields_validator.rb
+++ lib/morse_fields_validator/fields_validator.rb
@@ -1,7 +1,11 @@
 module FieldsValidator 
-  ERROR = 'please ensure necessary fields are in place'
 
   extend ActiveSupport::Concern
+
+  included do
+    validate :validate_column_names
+  end
+
   class_methods do 
     def validate_required_attributes
       required_attributes.each do |a|
@@ -9,17 +13,22 @@ module FieldsValidator
       end
     end
     def load_required_attributes(*_attrs)
-      @required_attributes ||=[]
-      @required_attributes += _attrs
+      @required_attributes=_attrs.any? ? _attrs : []
       @required_attributes.uniq!
     end
 
     def required_attributes
       @required_attributes
     end
+  end
 
-    def validate_column_names(*_attrs)
-      _attrs.each { |rdf| raise "#{rdf} not included. #{ERROR}" unless column_names.include?(rdf)}
-    end
+  private
+
+  def required_database_fields
+    defined?(super) ? super : []
+  end
+
+  def validate_column_names
+    required_database_fields.each { |rdf| errors_add(rdf, "does not have a column in the database.")  unless self.class.column_names.include?(rdf)} if required_database_fields.any?
   end
 end
--- spec/models/fields_validator_spec.rb
+++ spec/models/fields_validator_spec.rb
@@ -3,24 +3,53 @@ require 'spec_helper'
 RSpec.describe FieldsValidator, type: :module do
 
   class Thing 
+    include ActiveModel::Validations
+    def initialize
+      @errors={}
+    end
     def self.column_names
-      %w{name} 
+      [:name] 
+    end
+    def errors_add(k,v)
+      @errors[k]=v
     end
     include FieldsValidator
+  end
+
+  class ThingWithCorrectFields < Thing
+    def required_database_fields
+      [:name]
+    end
+  end
+
+  class ThingWithIncorrectFields < Thing
+    def required_database_fields
+      [:squirrels]
+    end
   end
 
   describe "when included" do
     it "should not get in the way of creation" do
       expect(Thing.new).to be_a(Thing)
+      expect(ThingWithIncorrectFields.new).to be_a(ThingWithIncorrectFields)
     end
   end
 
-  describe "validate_column_names" do 
-    it "should not squeak if column_names include input" do
-      expect{Thing.validate_column_names(*%w{name})}.to_not raise_error
-    end
-    it "should squeak if column_names do not include input" do
-      expect{Thing.validate_column_names(*%w{squirrels})}.to raise_error(RuntimeError)
+  describe "validations" do 
+    describe "validate_column_names" do 
+      context "where the correct fields are present" do
+        it "should be valid" do
+          t=ThingWithCorrectFields.new
+          expect(t).to be_valid
+        end
+      end
+      context "where the correct fields are not present" do
+        it "should not be valid" do
+          t=ThingWithIncorrectFields.new
+          expect(t).to_not be_valid
+          expect(t.errors).to have_key(:squirrels)
+        end
+      end
     end
   end
 end
